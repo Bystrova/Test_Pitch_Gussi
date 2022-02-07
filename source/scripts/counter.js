f0b42204c12ca8cc1ae1ea1f8346a4289bea5a0b
unknown <antonenko.a.b@gmail.com>
2022-02-07 13:45:04 +0300

    Добавлена доступность в форме заказа

--- source/scripts/counter.js
+++ source/scripts/counter.js
@@ -1,7 +1,7 @@
 const decreaseButton = document.querySelector('.control-button--decrease');
 const increaseButton = document.querySelector('.control-button--increase');
 const inputQuantity = document.querySelector('.form__input--quantity');
-const inputCost = document.querySelector('.form__input--cost');
+const inputHidden = document.querySelector('.form__input--hidden');
 const costInt = document.querySelector('.form__cost-integer');
 const costFloat = document.querySelector('.form__cost-float');
 const price = (costInt.innerHTML + costFloat.innerHTML).substring(1);
@@ -14,7 +14,7 @@ const changePrice = (quantity) => {
 	const lastIndex = firstIndex + 3;
 	costInt.innerHTML = '$' + String(Math.trunc(newPrice));
 	costFloat.innerHTML = String(newPrice).substring(firstIndex, lastIndex);
-	inputCost.value = newPrice;
+	inputHidden.value = newPrice;
 }
 
 decreaseButton.addEventListener('click', () => {
@@ -28,3 +28,7 @@ increaseButton.addEventListener('click', () => {
 	inputQuantity.value++;
 	changePrice(inputQuantity.value);
 });
+
+inputQuantity.addEventListener('input', () => {
+	changePrice(inputQuantity.value);
+})
